731aff866d350fbbe18a55634eee569ea3a3926a
Mattes <sharp1kdev@outlook.com>
2019-09-16 19:44:33 +0300

    - Update parser..

--- vkvid/__main__.py
+++ vkvid/__main__.py
@@ -17,13 +17,14 @@ except vk_api.AuthError as error:
 
 VK = vk_session.get_api()
 tools = vk_api.VkTools(vk=VK)
-params = dict(owner_id=group_id)
-videos = tools.get_all(method='video.get', max_count=8, values=params, limit=400)
-print('Loaded {} videos!'.format(videos['count']))
+values = {'owner_id': group_id}
+videos = tools.get_all_iter(method='video.get', max_count=8, values=values, limit=10000)
 
-with open('videos.txt', 'a') as file:
+print('Video loading is finished! Starting parse!')
+
+with open('videos.txt', 'a', encoding='utf-8') as file:
     try:
-        for vid in videos['items']:
+        for i, vid in enumerate(videos):
             link_str = 'https://vk.com/video{}_{}'.format(group_id, vid['id'])
             page = urlopen(link_str, timeout=5)
             content = page.read()
@@ -31,22 +32,21 @@ with open('videos.txt', 'a') as file:
             link = content.decode('utf-8', "ignore")
             string = re.compile('<source src=\\\\"([^"]*)\\\\"')
             urls = string.findall(link)
-            i = ['1080.mp4', '720.mp4', '360.mp4', '240.mp4']
-            if i in urls:
-                for url in urls:
-                    try:
-                        source = url.replace('\\/', '/')
-                        reg = re.compile(r'/([^/]*\.mp4)')
-                        name = reg.findall(source)[0]
-                        source = source.split('?')[0]
-                        file.write(source + "\n")
-                        print('Processing {}...'.format(source))
-                        break
-                    except Exception as e:
-                        print(e)
+            for url in urls:
+                if 'video_hls.php' not in url:
+                    if '1080.mp4' in url or '720.mp4' in url or '360.mp4' in url or '240.mp4':
+                        try:
+                            source = url.replace('\\/', '/')
+                            reg = re.compile(r'/([^/]*\.mp4)')
+                            name = reg.findall(source)[0]
+                            clear_link = source.split('?')[0]
+                            file.write(clear_link + "\n")
+                            print('[{}] Processing {}'.format(i, clear_link))
+                            break
+                        except Exception as e:
+                            print(e)
     except IncompleteRead as error:
         print(error)
-        pass
 
 print('Finish!')
 exit(0)
