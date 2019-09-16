b524e1a49ae17e98dabdcbae87fd6ef132aea0d9
Mattes <sharp1kdev@outlook.com>
2019-09-16 18:58:01 +0300

    - Add vk video

--- vkvid/__main__.py
+++ vkvid/__main__.py
@@ -18,32 +18,35 @@ except vk_api.AuthError as error:
 VK = vk_session.get_api()
 tools = vk_api.VkTools(vk=VK)
 params = dict(owner_id=group_id)
-videos = tools.get_all_iter(method='video.get', max_count=200, values=params, limit=10000)
+videos = tools.get_all(method='video.get', max_count=8, values=params, limit=400)
+print('Loaded {} videos!'.format(videos['count']))
 
 with open('videos.txt', 'a') as file:
     try:
-        for vid in videos:
-            print(vid)
+        for vid in videos['items']:
             link_str = 'https://vk.com/video{}_{}'.format(group_id, vid['id'])
             page = urlopen(link_str, timeout=5)
             content = page.read()
+            page.close()
             link = content.decode('utf-8', "ignore")
             string = re.compile('<source src=\\\\"([^"]*)\\\\"')
             urls = string.findall(link)
-            for i in ['1080.mp4', '720.mp4', '360.mp4', '240.mp4']:
+            i = ['1080.mp4', '720.mp4', '360.mp4', '240.mp4']
+            if i in urls:
                 for url in urls:
                     try:
-                        if i in url:
-                            source = url.replace('\\/', '/')
-                            reg = re.compile(r'/([^/]*\.mp4)')
-                            name = reg.findall(source)[0]
-                            source = source.split('?')[0]
-                            print(source)
-                            file.write(source + "\n")
-                            print('Processing {}...'.format(name))
-                            break
+                        source = url.replace('\\/', '/')
+                        reg = re.compile(r'/([^/]*\.mp4)')
+                        name = reg.findall(source)[0]
+                        source = source.split('?')[0]
+                        file.write(source + "\n")
+                        print('Processing {}...'.format(source))
+                        break
                     except Exception as e:
                         print(e)
     except IncompleteRead as error:
         print(error)
         pass
+
+print('Finish!')
+exit(0)
