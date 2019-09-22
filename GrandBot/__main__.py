d75236c55d1f839f4b4c2545ed4281a0cc76588d
Mattes <sharp1kdev@outlook.com>
2019-09-22 10:49:48 +0300

    - Sync

--- GrandBot/__main__.py
+++ GrandBot/__main__.py
@@ -2,26 +2,21 @@ import threading
 from datetime import datetime
 
 from GrandBot import BOT, LOGS
-from GrandBot import __copystring__, __version__
-from GrandBot.helpers import time_has_changed, convert_time_to_string, get_photos_id_list
+from GrandBot.helpers import time_has_changed, convert_time_to_string
 
 LOGS.info("Starting avatar changer...")
 BOT.start()
-ME = BOT.get_me().username
-LOGS.info(__copystring__)
-LOGS.info(f"You're logged in as \"{ME}\"!")
-LOGS.info(f"Your GrandBot is Version {__version__}\n")
 
 
 def change_picture(time_val):
     threading.Timer(1, change_picture, [convert_time_to_string(datetime.now()), ]).start()
     if time_has_changed(time_val):
         prev_time = convert_time_to_string(datetime.now())
-        my = BOT.get_profile_photos("me")
-        f_list = get_photos_id_list(my)
-        BOT.delete_profile_photos(f_list)
         BOT.set_profile_photo(f"time_images/{prev_time.replace(':', '_')}.jpg")
-        LOGS.info(f"Setting current profile pic to {prev_time}.jpg")
+        my_photos = iter(BOT.iter_profile_photos("me", offset=1))
+        for image in my_photos:
+            BOT.delete_profile_photos(image.file_id)
+        LOGS.info("Setting current avatar to {}.jpg".format(prev_time))
 
 
 change_picture("")
